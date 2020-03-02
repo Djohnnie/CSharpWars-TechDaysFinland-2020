531cd2a2f981d2d7476d55797cf8706b24e6da7d
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2020-03-02 12:29:52 +0100

    Initial commit

---
+++ src/backend/CSharpWars/CSharpWars.Web/Models/GameViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CSharpWars.Web.Models
+{
+    public class GameViewModel
+    {
+        public string PlayerName { get; set; }
+        public string SampleScript { get; set; }
+    }
+}
---
+++ src/backend/CSharpWars/CSharpWars.Web/Models/PlayViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpWars.Web.Models
+{
+    public class PlayViewModel
+    {
+        public string HappyMessage { get; set; }
+        public string SadMessage { get; set; }
+        public string PlayerName { get; set; }
+        public string BotName { get; set; }
+        public int BotHealth { get; set; }
+        public int BotStamina { get; set; }
+        public string Script { get; set; }
+        public Guid SelectedScript { get; set; }
+        public List<ScriptViewModel> Scripts { get; set; }
+    }
+}
